26def039aa0e7c05707ab002b4f9dc07dbb493e2
Jagrati Sharma <imjagratisharma@gmail.com>
2020-11-27 15:34:35 -0700

    New Dataset and Schema updated

--- project_schema.sql
+++ project_schema.sql
@@ -1,25 +1,38 @@
+-- This table combines the column which are in 3 of the 4 tabels 
+-- The last table i.e. vehicle collision has only zip code, but we 
+-- will get the county city and state from out additional dataset.
+
 CREATE TABLE collision_insight.location_data (
-	zipcode NUMERIC(5),
-	city TEXT,
-	county TEXT,
-	country TEXT,
-	countyfips NUMERIC(5),
+	zipcode NUMERIC(5), 
+	county VARCHAR(50),
+	city VARCHAR(85),
 	state VARCHAR(2),
-	state_fips NUMERIC(2),
-	latitude INTEGER,
-	longitude INTEGER,
 	PRIMARY KEY(zipcode)
 );
 
-CREATE TABLE collision_insight.geometric_data (
-	latitude NUMERIC,
-	longitude NUMERIC,
-	zipcode NUMERIC(5),
-	PRIMARY KEY(latitude, longitude), 
-	FOREIGN KEY(zipcode) REFERENCES location_data (zipcode)
-);
+
+-- Although our dataset has latitude and longitude, we are not considering it now.
+
+-- CREATE TABLE collision_insight.geometric_data (
+-- 	latitude INTEGER,
+-- 	longitude INTEGER,
+-- 	zipcode NUMERIC(5),
+-- 	PRIMARY KEY(latitude, longitude), 
+-- 	FOREIGN KEY(zipcode) REFERENCES location_data (zipcode)
+-- );
+
+
+-- https://hifld-geoplatform.opendata.arcgis.com/datasets/hospitals/data
+-- This is for USA and has 7596 rows
 
 CREATE TABLE collision_insight.hospital_details (
+	-- To remove these 4 column or keep them here in the schema 
+	-- instead of placing them in the combined location dataset
+	country TEXT,
+	countyfips NUMERIC(5),
+	state_id VARCHAR(10),
+	state_fips NUMERIC(2),
+
 
 	address VARCHAR(255),  
 	name VARCHAR(255), 
@@ -46,7 +59,7 @@ CREATE TABLE collision_insight.hospital_type (
 	FOREIGN KEY(id) REFERENCES hospital_details (id),
 );
 
-CREATE TABLE collision_insight.hospital_type (
+CREATE TABLE collision_insight.hospital_status (
 	id NUMERIC(10) PRIMARY KEY, 
 	status BOOLEAN,
 	FOREIGN KEY(id) REFERENCES hospital_details (id),
@@ -72,6 +85,11 @@ CREATE TABLE collision_insight.hospital_owner (
 	FOREIGN KEY(id) REFERENCES hospital_details (id),
 );
 
+
+-- https://data.ny.gov/Economic-Development/Liquor-Authority-Current-List-of-Active-Licenses/hrvs-fxs2
+-- This is for NY State and has 50.5K rows 
+-- Full county name
+
 CREATE TABLE collision_insight.liquor_shop_info (
 	
 	license_type_code VARCHAR(2), 
@@ -96,6 +114,11 @@ CREATE TABLE collision_insight.liquor_shop_extras (
 	FOREIGN KEY(serial_number) REFERENCES liquor_shop_info (serial_number)
 );
 
+
+-- https://data.ny.gov/Transportation/Vehicle-Repair-Shops-Across-New-York-State/icjc-x44x
+-- This is for NY State with 23.7K rows
+-- This dataset has county in 4 letters 
+
 CREATE TABLE collision_insight.vehicle_repair_info (
 	facility_id NUMERIC(7) PRIMARY KEY, 
 	facility_name VARCHAR(255), 
@@ -105,72 +128,60 @@ CREATE TABLE collision_insight.vehicle_repair_info (
 	owner_name_overflow TEXT,	
 	original_issuance DATE, 	
 	last_renewal DATE, 	
-	expiration DATE, 		
+	expiration DATE, 	 	
 	zipcode NUMERIC(5),
 	FOREIGN KEY(zipcode) REFERENCES location_data (zipcode)
 );
 
 CREATE TABLE collision_insight.vehicle_repair_business_type (
-	business_type BOOLEAN,
+	business_type BOOLEAN,	
+	-- RS and RSB types
 	facility_id NUMERIC(7) PRIMARY KEY, 
 	FOREIGN KEY(facility_id) REFERENCES vehicle_repair_info (facility_id)
 );
 
-CREATE TABLE collision_insight.vehicle_crashes (
-	serial_no BIGSERIAL PRIMARY KEY,
-	year YEAR, 	
-	time TIME, 	
-	date DATE,	
-	police_report BOOLEAN,
-	municipality TEXT,
-	traffic_control_device TEXT, 
-	dot_reference_marker_location VARCHAR(20), 			
-	number_of_vehicles_involved INTEGER,
+
+-- https://data.cityofnewyork.us/Public-Safety/Motor-Vehicle-Collisions-Crashes/h9gi-nx95
+-- This is for NYC only with 1.73M rows.
+
+CREATE TABLE collision_insight.vehicle_collision (
+	crash_date DATE,
+	crash_time TIME,
+	BORO VARCHAR,
+	on_street_name VARCHAR,
+	cross_street_name VARCHAR,
+	off_street_name VARCHAR,
+	person_injured INTEGER,
+	person_killed INTEGER,
+	pedistrian_injured INTEGER,
+	pedistrian_killed INTEGER,
+	cyclist_injured INTEGER,
+	cyclist_killed INTEGER,
+	motorist_injured INTEGER,
+	motorist_killed INTEGER,
+	collision_id NUMERIC(7) PRIMARY KEY;	
 	zipcode NUMERIC(5),
 	FOREIGN KEY(zipcode) REFERENCES location_data (zipcode)
 );
 
-CREATE TABLE collision_insight.vehicle_crashes_descriptor (
-	serial_no BIGSERIAL PRIMARY KEY,
-	crash_descriptor VARCHAR(63), 
-	FOREIGN KEY(serial_no) REFERENCES vehicle_crashes(serial_no)
+CREATE TABLE collision_insight.contributing_factor_vehicle (
+	contributing_factor_vehicle_1 VARCHAR(20),
+	contributing_factor_vehicle_2 VARCHAR(20),
+	contributing_factor_vehicle_3 VARCHAR(20),
+	contributing_factor_vehicle_4 VARCHAR(20),
+	contributing_factor_vehicle_5 VARCHAR(20),
+	collision_id NUMERIC(7) PRIMARY KEY;
+	FOREIGN KEY(collision_id) REFERENCES vehicle_collision (collision_id)
 );
 
-CREATE TABLE collision_insight.vehicle_crashes_day (
-	serial_no BIGSERIAL PRIMARY KEY,
-	day_of_week VARCHAR(9),
-	FOREIGN KEY(serial_no) REFERENCES vehicle_crashes(serial_no)
-);
-		
-CREATE TABLE collision_insight.vehicle_crashes_lighting (
-	serial_no BIGSERIAL PRIMARY KEY,
-	lighting_conditions VARCHAR(20),
-	FOREIGN KEY(serial_no) REFERENCES vehicle_crashes(serial_no)
-);
-
-CREATE TABLE collision_insight.vehicle_crashes_collision_desc (
-	serial_no BIGSERIAL PRIMARY KEY,
-	collision_type_descriptor VARCHAR(15), 		
-	FOREIGN KEY(serial_no) REFERENCES vehicle_crashes(serial_no)
-);
-
-CREATE TABLE collision_insight.vehicle_crashes_collision_weather (
-	serial_no BIGSERIAL PRIMARY KEY,
-	weather_conditions VARCHAR(30), 
-	FOREIGN KEY(serial_no) REFERENCES vehicle_crashes(serial_no)
-);
-	
-CREATE TABLE collision_insight.vehicle_crashes_collision_road_info (
-	serial_no BIGSERIAL PRIMARY KEY,
-	road_descriptor VARCHAR(30),
-	road_surface_conditions VARCHAR(15),		
-	FOREIGN KEY(serial_no) REFERENCES vehicle_crashes(serial_no)
-);
-
-CREATE TABLE collision_insight.vehicle_crashes_bicyclist (
-	serial_no BIGSERIAL PRIMARY KEY,
-	pedestrian_bicyclist_action VARCHAR(50),	
-	FOREIGN KEY(serial_no) REFERENCES vehicle_crashes(serial_no)
+CREATE TABLE collision_insight.vehicle_type (
+	vehicle_1 TEXT,
+	vehicle_2 TEXT,
+	vehicle_3 TEXT,
+	vehicle_4 TEXT,
+	vehicle_5 TEXT,
+	collision_id NUMERIC(7) PRIMARY KEY;
+	FOREIGN KEY(collision_id) REFERENCES vehicle_collision (collision_id)
 );
 
 
